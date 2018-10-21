9daebbd08af16a87c9d4629fa547279d6c8de5d8
Thantham (Judge) Madan <judge.madan@gmail.com>
2018-10-21 17:52:32 -0400

    Motor Control Update

--- pi/motor.py
+++ pi/motor.py
@@ -1,50 +1,56 @@
-#Receives motor data from PID, sends to Arduino
-
 #TODO: Receive data from PID (done over ROS)
 #TODO: Filter and process data to format easy for Arduino
 #TODO: Initialize GPIO
 #TODO: Send data over TX/RX to Arduino
 
-min_speed = -127 # backwards
-max_speed = 127 # forwards
+# NOTE: PREVIOUS CODE HAS BEEN COMMENTED OUT IN FAVOR OF move() as
+# we want to be able to compute motor power synchronously.
 
-##################
-# BASE FUNCTIONS #
-##################
-
-def move_xy(power=max_speed, angle=0):
+def move(x, y, z, roll, pitch, yaw):
     pass
 
-def move_z(power=max_speed):
-    pass
+# PREVIOUS CODE COMMENTED OUT BELOW
 
-def roll(power=max_speed):
-    pass
-
-def pitch(power=max_speed):
-    pass
-
-def rotate(power=max_speed):
-    pass
-
-def yaw(power=max_speed): # if preferred
-    rotate(power)
-
-#################
-# SUPPLEMENTALS #
-#################
-
-def roll_deg(power=max_speed, angle):
-    pass
-
-def pitch_deg(power=max_speed, angle):
-    pass
-
-def move_z_fixed(power=max_speed, distance):
-    pass
-
-def move_xy_fixed(power=max_speed, distance):
-    pass
-
-def rotate_deg(power=max_speed, angle):
-    pass
+# min_speed = -127 # backwards
+# max_speed = 127 # forwards
+#
+# ##################
+# # BASE FUNCTIONS #
+# ##################
+#
+# def move_xy(power=max_speed, angle=0):
+#     pass
+#
+# def move_z(power=max_speed):
+#     pass
+#
+# def roll(power=max_speed):
+#     pass
+#
+# def pitch(power=max_speed):
+#     pass
+#
+# def rotate(power=max_speed):
+#     pass
+#
+# def yaw(power=max_speed): # if preferred
+#     rotate(power)
+#
+# #################
+# # SUPPLEMENTALS #
+# #################
+#
+# def roll_deg(power=max_speed, angle):
+#     pass
+#
+# def pitch_deg(power=max_speed, angle):
+#     pass
+#
+# def move_z_fixed(power=max_speed, distance):
+#     pass
+#
+# def move_xy_fixed(power=max_speed, distance):
+#     pass
+#
+# def rotate_deg(power=max_speed, angle):
+#     pass
